84119612999dcd3a5964aa11b58c1d3d14d02e48
kauassilva <kauassilva1@gmail.com>
2024-07-03 15:34:58 -0300

    Initial commit

---
+++ src/main/java/com/api/mantask/MantaskApplication.java
@@ -0,0 +1,13 @@
+package com.api.mantask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/mantask/MantaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.mantask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
